5d518b618e8871353fccc5c884b29a3ce1d98868
Hector <hborras@cyberclick.net>
2018-01-11 21:43:38 +0100

    Updated promoted account fields

--- src/TwitterAds/Fields/PromotedAccountFields.php
+++ src/TwitterAds/Fields/PromotedAccountFields.php
@@ -3,21 +3,9 @@
 namespace Hborras\TwitterAdsSDK\TwitterAds\Fields;
 
 
-/**
- * Created by PhpStorm.
- * User: hborras
- * Date: 12/05/17
- * Time: 18:19
- */
-class PromotedAccountFields
+class PromotedAccountFields extends Fields
 {
-    const ID                 = 'id';
-    const CREATED_AT         = 'created_at';
-    const UPDATED_AT         = 'updated_at';
-    const DELETED            = 'delete';
     const APPROVAL_STATUS    = 'approval_status';
     const LINE_ITEM_ID       = 'line_item_id';
     const USER_ID            = 'user_id';
-    const PAUSED             = 'paused';
-    const WITH_TOTAL_COUNT   = 'with_total_count';
 }
