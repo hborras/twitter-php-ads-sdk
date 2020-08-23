cfbc280f3be2ae169a1c81ea577b7da9642bfddd
Héctor Borrás <hborras@cyberclick.net>
2020-08-23 22:20:02 +0200

    Added net Placement values

--- src/TwitterAds/Object/Values/PlacementValues.php
+++ src/TwitterAds/Object/Values/PlacementValues.php
@@ -8,6 +8,11 @@ use Hborras\TwitterAdsSDK\TwitterAds\Enum\AbstractEnum;
 
 class PlacementValues extends AbstractEnum
 {
-    const ALL_ON_TWITTER    = 'ALL_ON_TWITTER';
-    const PUBLISHER_NETWORK = 'PUBLISHER_NETWORK';
+    const ALL_ON_TWITTER     = 'ALL_ON_TWITTER';
+    const PUBLISHER_NETWORK  = 'PUBLISHER_NETWORK';
+    const TAP_FULL           = 'TAP_FULL';
+    const TAP_FULL_LANDSCAPE = 'TAP_FULL_LANDSCAPE';
+    const TAP_BANNER         = 'TAP_BANNER';
+    const TAP_NATIVE         = 'TAP_NATIVE';
+    const TAP_MRECT          = 'TAP_MRECT';
 }
